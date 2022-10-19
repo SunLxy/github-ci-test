2edeb606b6099077635f51b0c012657fcc8e63a1
SunLxy <1011771396@qq.com>
2022-10-19 22:17:41 +0800

    fix:测试

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
-const a = 11;
-console.log(a)
+const pacote = require('pacote')
+const path = require("path")
+const libpub = require('libnpmpublish')
+
+const dadsa = path.join(__dirname, "../packages/demo1")
+
+const run = async () => {
+  const manifest = await pacote.manifest(dadsa)
+  const tarData = await pacote.tarball(dadsa)
+  console.log(manifest, tarData)
+  await libpub.publish(manifest, tarData, {
+    npmVersion: 'github-ci-test-demo1@1.0.10-bate-39',
+    defaultTag: "bate",
+    token: "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"
+  })
+}
+run()
